dd2c3b84f124a944605514bcf30cac2fa6617ce9
PHENGSIAROUN Dany <dphengsiaroun@alliance.net>
2018-10-17 17:51:26 +0200

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,36 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
-import { HomeComponent } from './components/views/home/home.component';
-import { SliderComponent } from './components/slider/slider.component';
-import { BannerComponent } from './components/banner/banner.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { SidebarCartComponent } from './components/sidebar-cart/sidebar-cart.component';
-import { SidebarNavComponent } from './components/sidebar-nav/sidebar-nav.component';
-import { ModalProductComponent } from './components/modal-product/modal-product.component';
-import { BackToTopComponent } from './components/back-to-top/back-to-top.component';
-import { ModalSearchComponent } from './components/modal-search/modal-search.component';
+import { HomeComponent } from './components/screens/home/home.component';
+import { SliderComponent } from './components/ui/slider/slider.component';
+import { BannerComponent } from './components/ui/banner/banner.component';
+import { ProductListComponent } from './components/ui/product-list/product-list.component';
+import { SidebarCartComponent } from './components/ui/sidebar-cart/sidebar-cart.component';
+import { SidebarNavComponent } from './components/ui/sidebar-nav/sidebar-nav.component';
+import { ModalProductComponent } from './components/ui/modal-product/modal-product.component';
+import { BackToTopComponent } from './components/ui/back-to-top/back-to-top.component';
+import { ModalSearchComponent } from './components/ui/modal-search/modal-search.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    SliderComponent,
-    BannerComponent,
-    ProductListComponent,
-    SidebarCartComponent,
-    SidebarNavComponent,
-    ModalProductComponent,
-    BackToTopComponent,
-    ModalSearchComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		HomeComponent,
+		SliderComponent,
+		BannerComponent,
+		ProductListComponent,
+		SidebarCartComponent,
+		SidebarNavComponent,
+		ModalProductComponent,
+		BackToTopComponent,
+		ModalSearchComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
