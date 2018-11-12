bed3c8afccd7fc24d4deda876cb3fcd6666c301e
PHENGSIAROUN Dany <dphengsiaroun@alliance.net>
2018-11-12 10:19:47 +0100

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { Router, NavigationEnd } from '@angular/router';
-import "animsition";
 
 @Component({
   selector: "app-root",
@@ -10,30 +8,5 @@ import "animsition";
 export class AppComponent implements OnInit {
   title = "ng6-shop";
 
-  constructor(private router: Router) { }
-
-  ngOnInit() {
-    // (<any>$(".animsition")).animsition({
-    //   inClass: "fade-in",
-    //   outClass: "fade-out",
-    //   inDuration: 1500,
-    //   outDuration: 800,
-    //   linkElement: ".animsition-link",
-    //   loading: true,
-    //   loadingParentElement: "html",
-    //   loadingClass: "animsition-loading-1",
-    //   loadingInner: '<div class="loader05"></div>',
-    //   timeout: false,
-    //   timeoutCountdown: 5000,
-    //   onLoadEvent: true,
-    //   browser: ["animation-duration", "-webkit-animation-duration"],
-    //   overlay: false,
-    //   overlayClass: "animsition-overlay-slide",
-    //   overlayParentElement: "html",
-    //   transition: function(url) {
-    //     window.location.href = url;
-    //   }
-    // });
-
-  }
+  ngOnInit() {}
 }
